35466204fa272e2af05bbb1e5a9569f219c2b4d1
kinsaf <kinsaf@gmail.com>
2023-05-18 08:24:05 +0200

    Ajout Tests Unitaires Pour les modèles

--- mercadona_back/tests.py
+++ mercadona_back/tests.py
@@ -1,3 +1,31 @@
+import datetime
 from django.test import TestCase
+from mercadona_back.models import Catégorie
+from mercadona_back.models import ImageProduit
+from mercadona_back.models import Promotion
+from mercadona_back.models import Produit
 
 # Create your tests here.
+class CategorieModelTest(TestCase):
+
+    def test_string_representation(self):
+        categ = Catégorie(libelle="Mon libelle")
+        self.assertEqual(str(categ), categ.libelle)
+
+class ImageModelTest(TestCase):
+
+    def test_string_representation(self):
+        image = ImageProduit(Nom_Image_Produit="Image")
+        self.assertEqual(str(image), image.Nom_Image_Produit)
+
+class PromotionModelTest(TestCase):
+
+    def test_string_representation(self):
+        promo = Promotion(Nom="Ma Promotion")
+        self.assertEqual(str(promo), promo.Nom)
+
+class ProduitModelTest(TestCase):
+
+    def test_string_representation(self):
+        produit = Produit(libelle="Mon produit")
+        self.assertEqual(str(produit), produit.libelle)
